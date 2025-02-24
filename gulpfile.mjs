ef2da464cd3cbbc6f5a5a52fc3228382e022df65
mwood77 <43637076+mwood77@users.noreply.github.com>
2025-02-24 10:34:15 +0100

    update build process

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,7 +1,9 @@
 import updateVendor from './gulp/update-vendor.mjs';
-import publishFrontend from './gulp/publish-frontend.mjs';
+import publishFrontend from './gulp/build-gh-pages.mjs';
 
 export {
 	updateVendor,
 	publishFrontend,
 };
+
+export default publishFrontend;
